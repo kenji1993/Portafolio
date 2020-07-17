388887d78f6802c47b00fd4e2d6ecdb3aa98a10a
Kenji <kenji.arakaki93@gmail.com>
2020-07-17 08:41:21 -0300

    Commit con dos paginas en el portafolio

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const typed = new Typed('.typed', {
+	strings: ['Front-end Designer', 'Front-end Developer'],
+	typeSpeed: 90,
+	startDelay: 300,
+	backSpeed: 90,
+	smartBackspace: true,
+	loop: true,
+});
