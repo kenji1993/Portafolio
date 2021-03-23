ed8e21b38fb19476621c2d20609bafc9bcb8fc7a
Kenji <kenji.arakaki93@gmail.com>
2021-03-23 18:33:23 -0300

    Agregado de galeria y actualizacion de datos en el home

--- js/particulas.js
+++ js/particulas.js
@@ -22,7 +22,7 @@ particlesJS({
             "image": {
                 "src": "img/github.svg",
                 "width": 100,
-                "height": 100
+                "height": 100,
             }
         },
         "opacity": {
